ac85fb1205dd0b7c3313e89db75fa3812f965be4
Caio-Oliveira98 <caio.s.oliveira@icloud.com>
2024-09-16 22:47:45 -0300

    Add files via upload

---
+++ criando funções.R
@@ -0,0 +1,42 @@
+#Criando funções
+
+x = 10
+x
+
+x+3
+
+
+#Aprendendo Vetores:
+vetor1 = 1:7
+
+vetor2 = c(5 , 3 , 7 , 10 , -5)
+
+vetor3 = c("Gabriel", "Júlia", "Caio", "Fabrício")
+
+
+
+vetor4 = array(1:7)
+
+vetor3[2]
+
+vetor3[1:4]
+
+#Função de Soma
+sum(vetor2)
+
+#função de produto
+
+prod(vetor2)
+
+#soma ou subtração de vetores(os vetores tem que ter a mesma dimensão)
+
+vetor5 = c(10, 3.14, -5, 12, 2/5)
+vetor2 + vetor5
+
+vetor5 - vetor2
+
+vetor5 * vetor2
+
+vetor2 / vetor2
+
+

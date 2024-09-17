dd59e82571283aa2b4e52c326dd04716f86bc349
Caio-Oliveira98 <caio.s.oliveira@icloud.com>
2024-09-16 22:19:08 -0300

    Add files via upload

---
+++ fórmulas R.R
@@ -0,0 +1,39 @@
+#operação de Soma:
+5+3
+
+#operção de subtração:
+10-15
+
+#multiplicação:
+2*6
+
+#divisão:
+400/10
+
+#potenciação:
+5^2
+#ou:
+5**2
+
+#raiz quadrada:
+sqrt(100)
+
+#raiz enésima:
+4^(1/2)
+
+8^(1/3)
+
+#módulo:
+abs(-5)
+
+#resto da divisão:
+20%%5
+
+#quociente da divisão:
+20%/%5
+
+#fatorial:
+factorial(5)
+
+#logaritmo:
+log(16,2)
